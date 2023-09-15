0077e5d9d5cb45c81d6a98939c7cf1a59887363a
riorio805 <sefrianojieftara@gmail.com>
2023-09-15 17:35:34 +0700

    try

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -17,7 +17,6 @@ import os # Tambahkan kode berikut
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 env = environ.Env() # Tambahkan kode berikut
 
 
