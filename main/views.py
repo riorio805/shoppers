bea6317f99443d6b97b45d0e83e73285d04a8f70
riorio805 <sefrianojieftara@gmail.com>
2023-09-20 12:10:56 +0700

    tutorial 3 done

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages  
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core import serializers
 from django.urls import reverse
@@ -6,13 +11,15 @@ from main.forms import ProductForm
 from main.models import Product
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     products = Product.objects.all()
 
     context = {
-        'name': 'sefriano', # Nama kamu
+        'name': request.user.username, # Nama kamu
         'class': 'PBP D', # Kelas PBP kamu
-        'products': products
+        'products': products,
+        'last_login': request.COOKIES['last_login']
     }
 
     return render(request, "main.html", context)
@@ -21,11 +28,49 @@ def create_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()), max_age=10000)
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
 
 def show_xml(request):
     data = Product.objects.all()
