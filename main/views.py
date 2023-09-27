3374a750dcf1e7a8eb1851bb964679e8a50554c4
riorio805 <sefrianojieftara@gmail.com>
2023-09-27 14:24:07 +0700

    tutorial 4 done + extras

--- main/views.py
+++ main/views.py
@@ -19,11 +19,12 @@ def show_main(request):
         'name': request.user.username, # Nama kamu
         'class': 'PBP D', # Kelas PBP kamu
         'products': products,
-        'last_login': request.COOKIES['last_login']
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
 
+@login_required(login_url='/login')
 def create_product(request):
     form = ProductForm(request.POST or None)
 
@@ -35,6 +36,32 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+@login_required(login_url='/login')
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+@login_required(login_url='/login')
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 
 
 def register(request):
