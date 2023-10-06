2cb82c5865539a6472d02e99a566338140db6861
riorio805 <sefrianojieftara@gmail.com>
2023-10-06 09:42:24 +0700

    changes

--- main/views.py
+++ main/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages  
-from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 from main.forms import ProductForm
 from main.models import Product
 
@@ -89,7 +90,9 @@ def login_user(request):
             return response
         else:
             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+
     context = {}
+    print(request.user.username)
     return render(request, 'login.html', context)
 
 def logout_user(request):
@@ -114,3 +117,23 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
